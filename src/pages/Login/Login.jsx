6b27c6352b4acfd39acffb3fdc8c4676c5c10134
kazimehedihasan <kazimehedihasan458@.com>
2023-10-03 23:17:02 +0600

    creat browser

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar/>
+            <h3>\login</h3>
+        </div>
+    );
+};
+
+export default Login;
