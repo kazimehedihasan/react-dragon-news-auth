3353952a6331ddc1558eceb12a77f4295889fd34
kazimehedihasan <kazimehedihasan458@.com>
2023-10-09 01:24:47 +0600

    login registra

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey:import.meta.env. VITE_APIKEY,
-  authDomain:import.meta.env.VITE_AUTHDOMAIN ,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket:import.meta.env.VITE_STORAGEBUCKET ,
-  messagingSenderId:import.meta.env. VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID
+  apiKey: "AIzaSyD5t8sldRyoEzuicQC5GxlqUtopigFDrWY",
+  authDomain: "react-dragon-news-auth.firebaseapp.com",
+  projectId: "react-dragon-news-auth",
+  storageBucket: "react-dragon-news-auth.appspot.com",
+  messagingSenderId: "750044483429",
+  appId: "1:750044483429:web:77f4f5d3f33234b21f5b8c"
 };
 
 
